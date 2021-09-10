5fe3b49262e54c2c54220f49aa9fb2057cd6bd43
pethzero <thanadol6@gmail.com>
2021-09-10 19:28:56 +0700

    UPLOAD COLOR

--- ProjectFrameVB/FormVB.Designer.vb
+++ ProjectFrameVB/FormVB.Designer.vb
@@ -40,9 +40,10 @@ Partial Class frmMain
         '
         Me.btnSave.Font = New System.Drawing.Font("Leelawadee UI", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnSave.Image = Global.ProjectFrameVB.My.Resources.Resources.download
-        Me.btnSave.Location = New System.Drawing.Point(596, 88)
+        Me.btnSave.Location = New System.Drawing.Point(447, 72)
+        Me.btnSave.Margin = New System.Windows.Forms.Padding(2)
         Me.btnSave.Name = "btnSave"
-        Me.btnSave.Size = New System.Drawing.Size(200, 76)
+        Me.btnSave.Size = New System.Drawing.Size(150, 62)
         Me.btnSave.TabIndex = 0
         Me.btnSave.Text = "บันทึกข้อมูล"
         Me.btnSave.UseVisualStyleBackColor = True
@@ -51,19 +52,21 @@ Partial Class frmMain
         '
         Me.lblShowText.AutoSize = True
         Me.lblShowText.Font = New System.Drawing.Font("Leelawadee", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblShowText.ForeColor = System.Drawing.Color.Blue
-        Me.lblShowText.Location = New System.Drawing.Point(195, 39)
+        Me.lblShowText.ForeColor = System.Drawing.Color.Black
+        Me.lblShowText.Location = New System.Drawing.Point(146, 32)
+        Me.lblShowText.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.lblShowText.Name = "lblShowText"
-        Me.lblShowText.Size = New System.Drawing.Size(82, 29)
+        Me.lblShowText.Size = New System.Drawing.Size(56, 23)
         Me.lblShowText.TabIndex = 1
-        Me.lblShowText.Text = "Label1"
+        Me.lblShowText.Text = "Color"
         '
         'btnClose
         '
         Me.btnClose.BackColor = System.Drawing.SystemColors.MenuHighlight
-        Me.btnClose.Location = New System.Drawing.Point(596, 430)
+        Me.btnClose.Location = New System.Drawing.Point(447, 349)
+        Me.btnClose.Margin = New System.Windows.Forms.Padding(2)
         Me.btnClose.Name = "btnClose"
-        Me.btnClose.Size = New System.Drawing.Size(200, 67)
+        Me.btnClose.Size = New System.Drawing.Size(150, 54)
         Me.btnClose.TabIndex = 2
         Me.btnClose.Text = "ปิดโปรแกรม"
         Me.btnClose.UseVisualStyleBackColor = False
@@ -71,17 +74,19 @@ Partial Class frmMain
         'txtInputText
         '
         Me.txtInputText.Font = New System.Drawing.Font("Leelawadee", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtInputText.Location = New System.Drawing.Point(187, 114)
+        Me.txtInputText.Location = New System.Drawing.Point(140, 93)
+        Me.txtInputText.Margin = New System.Windows.Forms.Padding(2)
         Me.txtInputText.Name = "txtInputText"
-        Me.txtInputText.Size = New System.Drawing.Size(373, 28)
+        Me.txtInputText.Size = New System.Drawing.Size(281, 24)
         Me.txtInputText.TabIndex = 3
         '
         'btnClearText
         '
         Me.btnClearText.Font = New System.Drawing.Font("Leelawadee UI", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnClearText.Location = New System.Drawing.Point(596, 330)
+        Me.btnClearText.Location = New System.Drawing.Point(447, 268)
+        Me.btnClearText.Margin = New System.Windows.Forms.Padding(2)
         Me.btnClearText.Name = "btnClearText"
-        Me.btnClearText.Size = New System.Drawing.Size(200, 67)
+        Me.btnClearText.Size = New System.Drawing.Size(150, 54)
         Me.btnClearText.TabIndex = 4
         Me.btnClearText.Text = "ลบช้อมูล"
         Me.btnClearText.UseVisualStyleBackColor = True
@@ -90,17 +95,19 @@ Partial Class frmMain
         '
         Me.ListBox1.Font = New System.Drawing.Font("Leelawadee", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.ListBox1.FormattingEnabled = True
-        Me.ListBox1.ItemHeight = 20
-        Me.ListBox1.Location = New System.Drawing.Point(187, 181)
+        Me.ListBox1.ItemHeight = 16
+        Me.ListBox1.Location = New System.Drawing.Point(140, 147)
+        Me.ListBox1.Margin = New System.Windows.Forms.Padding(2)
         Me.ListBox1.Name = "ListBox1"
-        Me.ListBox1.Size = New System.Drawing.Size(373, 164)
+        Me.ListBox1.Size = New System.Drawing.Size(281, 132)
         Me.ListBox1.TabIndex = 5
         '
         'btnChooseColor
         '
-        Me.btnChooseColor.Location = New System.Drawing.Point(34, 107)
+        Me.btnChooseColor.Location = New System.Drawing.Point(26, 87)
+        Me.btnChooseColor.Margin = New System.Windows.Forms.Padding(2)
         Me.btnChooseColor.Name = "btnChooseColor"
-        Me.btnChooseColor.Size = New System.Drawing.Size(75, 36)
+        Me.btnChooseColor.Size = New System.Drawing.Size(56, 29)
         Me.btnChooseColor.TabIndex = 6
         Me.btnChooseColor.Text = "เลือกสี"
         Me.btnChooseColor.UseVisualStyleBackColor = True
@@ -108,9 +115,10 @@ Partial Class frmMain
         'CheckBox1
         '
         Me.CheckBox1.AutoSize = True
-        Me.CheckBox1.Location = New System.Drawing.Point(34, 183)
+        Me.CheckBox1.Location = New System.Drawing.Point(26, 149)
+        Me.CheckBox1.Margin = New System.Windows.Forms.Padding(2)
         Me.CheckBox1.Name = "CheckBox1"
-        Me.CheckBox1.Size = New System.Drawing.Size(66, 21)
+        Me.CheckBox1.Size = New System.Drawing.Size(58, 17)
         Me.CheckBox1.TabIndex = 7
         Me.CheckBox1.Text = "แตงโม"
         Me.CheckBox1.UseVisualStyleBackColor = True
@@ -118,9 +126,10 @@ Partial Class frmMain
         'CheckBox2
         '
         Me.CheckBox2.AutoSize = True
-        Me.CheckBox2.Location = New System.Drawing.Point(34, 211)
+        Me.CheckBox2.Location = New System.Drawing.Point(26, 171)
+        Me.CheckBox2.Margin = New System.Windows.Forms.Padding(2)
         Me.CheckBox2.Name = "CheckBox2"
-        Me.CheckBox2.Size = New System.Drawing.Size(72, 21)
+        Me.CheckBox2.Size = New System.Drawing.Size(62, 17)
         Me.CheckBox2.TabIndex = 8
         Me.CheckBox2.Text = "มะละกอ"
         Me.CheckBox2.UseVisualStyleBackColor = True
@@ -128,9 +137,10 @@ Partial Class frmMain
         'CheckBox3
         '
         Me.CheckBox3.AutoSize = True
-        Me.CheckBox3.Location = New System.Drawing.Point(34, 239)
+        Me.CheckBox3.Location = New System.Drawing.Point(26, 194)
+        Me.CheckBox3.Margin = New System.Windows.Forms.Padding(2)
         Me.CheckBox3.Name = "CheckBox3"
-        Me.CheckBox3.Size = New System.Drawing.Size(66, 21)
+        Me.CheckBox3.Size = New System.Drawing.Size(57, 17)
         Me.CheckBox3.TabIndex = 9
         Me.CheckBox3.Text = "มะนาว"
         Me.CheckBox3.UseVisualStyleBackColor = True
@@ -138,18 +148,19 @@ Partial Class frmMain
         'CheckBox4
         '
         Me.CheckBox4.AutoSize = True
-        Me.CheckBox4.Location = New System.Drawing.Point(34, 267)
+        Me.CheckBox4.Location = New System.Drawing.Point(26, 217)
+        Me.CheckBox4.Margin = New System.Windows.Forms.Padding(2)
         Me.CheckBox4.Name = "CheckBox4"
-        Me.CheckBox4.Size = New System.Drawing.Size(59, 21)
+        Me.CheckBox4.Size = New System.Drawing.Size(52, 17)
         Me.CheckBox4.TabIndex = 10
         Me.CheckBox4.Text = "กล้วย"
         Me.CheckBox4.UseVisualStyleBackColor = True
         '
         'frmMain
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(818, 541)
+        Me.ClientSize = New System.Drawing.Size(614, 440)
         Me.Controls.Add(Me.CheckBox4)
         Me.Controls.Add(Me.CheckBox3)
         Me.Controls.Add(Me.CheckBox2)
@@ -161,6 +172,7 @@ Partial Class frmMain
         Me.Controls.Add(Me.btnClose)
         Me.Controls.Add(Me.lblShowText)
         Me.Controls.Add(Me.btnSave)
+        Me.Margin = New System.Windows.Forms.Padding(2)
         Me.Name = "frmMain"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "ทดสอบโปรแกรม"
--- ProjectFrameVB/FormVB.vb
+++ ProjectFrameVB/FormVB.vb
@@ -2,13 +2,20 @@
 
     Dim vProgram As String = "ทดสอบ"
     Dim tmpSelect As Integer
-    Dim tmpColor As ColorDialog
+
+    Dim MyColors As List(Of Color) = New List(Of Color)()
+
+    'Dim MyColors() As Color
+    'Dim ColorsElements As Integer = 0
+    Dim tmpColor As Color = Color.Black
     Dim myStrings() As String
     Dim myStringElements As Integer = 0
-
+    Dim processdraw As Integer = 0
+    Dim index As Integer
     Private Sub FormVB_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'lblShowText.Text = String.Empty
         txtInputText.Text = String.Empty
+        ListBox1.DrawMode = DrawMode.OwnerDrawFixed
     End Sub
 
     Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
@@ -20,38 +27,64 @@
     End Sub
 
     Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        'If txtInputText.Text = String.Empty Then
+        '    MessageBox.Show("กรุณาบันทึกข้อมูล")
+        '    Exit Sub
+        'Else
+        '    Dim result As DialogResult = MessageBox.Show("ยืนยันบันทึกข้อมูล", vProgram, MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        '    If result = vbYes Then
+        '        lblShowText.Text = txtInputText.Text
+        '        ListBox1.Items.Add(txtInputText.Text)
+
+
+        '        lblShowText.Text = String.Empty
+        '        txtInputText.Text = String.Empty
+        '        'btnSave.BackColor = Color.Red
+
+        '    End If
+        '    Exit Sub
+        'End If
+
+
         If txtInputText.Text = String.Empty Then
-            MessageBox.Show("กรุณาบันทึกข้อมูล")
-            Exit Sub
-        Else
-            Dim result As DialogResult = MessageBox.Show("ยืนยันบันทึกข้อมูล", vProgram, MessageBoxButtons.YesNo, MessageBoxIcon.Question)
-            If result = vbYes Then
-                lblShowText.Text = txtInputText.Text
-                ListBox1.Items.Add(txtInputText.Text)
-
-
-                lblShowText.Text = String.Empty
-                txtInputText.Text = String.Empty
-                btnSave.BackColor = Color.Red
-
-            End If
             Exit Sub
         End If
+
+
+        ListBox1.Items.Add(txtInputText.Text)
+        MyColors.Add(tmpColor)
+        ListBox1.Invalidate()
+
+        'AddElementToColorArray(tmpColor)
+        'ListBox1.Invalidate()
+
+        'ListBox1.DrawItem += New DrawItemEventHandler(ListBox1_DrawItem)
+        Console.Write(MyColors)
+
+
+        'ListBox1.ForeColor = tmpColor
+        'lblShowText.Text = String.Empty
+
+        lblShowText.ForeColor = tmpColor
+        txtInputText.Text = String.Empty
     End Sub
 
     Private Sub btnClearText_Click(sender As Object, e As EventArgs) Handles btnClearText.Click
-        If tmpSelect < 0 Then
+        ListBox1.Invalidate()
+        If ListBox1.Items.Count <= 0 Then
             Exit Sub
         End If
-        ListBox1.Items.RemoveAt(tmpSelect)
-        btnSave.BackColor = Color.Gray
-        lblShowText.Text = String.Empty
+
+        MyColors.RemoveAt(0)
+        ListBox1.Items.RemoveAt(0)
         txtInputText.Text = String.Empty
     End Sub
 
     Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged, ListBox1.Click
         'MessageBox.Show(ListBox1.SelectedItem)
         tmpSelect = ListBox1.SelectedIndex
+        ListBox1.Invalidate()
+
         'MessageBox.Show(ListBox1.SelectedIndex)
     End Sub
 
@@ -60,39 +93,95 @@
         If tmpSelect < 0 Then
             Exit Sub
         End If
+        MyColors.RemoveAt(tmpSelect)
         ListBox1.Items.RemoveAt(tmpSelect)
-        lblShowText.Text = String.Empty
         txtInputText.Text = String.Empty
+        ListBox1.Invalidate()
     End Sub
 
     Private Sub btnChooseColor_Click(sender As Object, e As EventArgs) Handles btnChooseColor.Click
         'ColorDialog1.ShowDialog()
         If ColorDialog1.ShowDialog <> DialogResult.Cancel Then
             lblShowText.ForeColor = ColorDialog1.Color
-            txtInputText.ForeColor = ColorDialog1.Color
+            'txtInputText.ForeColor = ColorDialog1.Color
+            tmpColor = ColorDialog1.Color
+            Console.Write(tmpColor)
         End If
     End Sub
 
     Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
-        'If CheckBox1.Checked = True Then
-        '    ListBox1.Items.Add(CheckBox1.Text)
-        'Else
-        '    ListBox1.Items.RemoveAt(tmpSelect)
-        'End If
-        Dim i As Integer
-        For i = 1 To 5
-            AddElementToStringArray("MyString " & i & ".")
-        Next
-
-        Console.Write(myStrings)
-        Me.lblShowText.Text = ("Number of elements in array is: " & myStrings.Length)
 
     End Sub
+
 
     Public Sub AddElementToStringArray(ByVal stringToAdd As String)
         ReDim Preserve myStrings(myStringElements)
         myStrings(myStringElements) = stringToAdd
         myStringElements += 1
     End Sub
+
+
+    'Add Color
+    'Public Sub AddElementToColorArray(ByVal ColorToAdd As Color)
+    '    ReDim Preserve MyColors(ColorsElements)
+    '    MyColors(ColorsElements) = ColorToAdd
+    '    ColorsElements += 1
+    'End Sub
+
+
+
+
+
+    'Change Color
+    Private Sub ListBox1_DrawItem(ByVal sender As Object, ByVal e As System.Windows.Forms.DrawItemEventArgs) Handles ListBox1.DrawItem
+
+        e.DrawBackground()
+        If ListBox1.Items.Count > 0 Then
+            e.Graphics.DrawString(TryCast(sender, ListBox).Items(e.Index).ToString, e.Font, New SolidBrush(MyColors(e.Index)), e.Bounds)
+        End If
+        e.DrawFocusRectangle()
+
+
+    End Sub
+
+    'Private Sub ListBox1_DrawItem(ByVal sender As Object, ByVal e As System.Windows.Forms.DrawItemEventArgs) Handles ListBox1.DrawItem
+    '    e.DrawBackground()
+    '    If e.Index Mod 2 <> 0 Then 'paint even elements with red
+    '        e.Graphics.DrawString(TryCast(sender, ListBox).Items(e.Index).ToString, e.Font, New SolidBrush(Color.Red), e.Bounds)
+    '    Else
+    '        e.Graphics.DrawString(TryCast(sender, ListBox).Items(e.Index).ToString, e.Font, New SolidBrush(Color.Black), e.Bounds)
+    '    End If
+    'End Sub
+    'Private Sub ListBox1_DrawItem(sender As Object, e As DrawItemEventArgs) Handles ListBox1.DrawItem
+    '    '//Draw the background of the ListBox control for each item.
+    '    '// Create a new Brush and initialize to a Black colored brush
+    '    '// by default.
+    '    e.DrawBackground()
+    '    Dim mybrush As Brush = Brushes.Black
+
+    '    '// Determine the color of the brush to draw each item based on 
+    '    '//the index of the item to draw.
+    '    Select Case ListBox1.Items(e.Index).ToString
+    '        Case "Error"
+    '            mybrush = Brushes.Red
+    '        Case "Ready"
+    '            mybrush = Brushes.Blue
+    '        Case "Success"
+    '            mybrush = Brushes.Green
+    '    End Select
+
+    '    '//
+    '    '// Draw the current item text based on the current 
+    '    '// Font and the custom brush settings.
+    '    '//
+    '    e.Graphics.DrawString(ListBox1.Items(e.Index).ToString(),
+    '                          e.Font, mybrush, e.Bounds, StringFormat.GenericDefault)
+    '    '//
+    '    '// If the ListBox has focus, draw a focus rectangle 
+    '    '// around the selected item.
+    '    '//
+    '    e.DrawFocusRectangle()
+    'End Sub
+
 
 End Class

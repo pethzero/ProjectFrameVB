f81e2b90a915c9e027fb49bfb9f9689684f5e000
pethzero <thanadol6.com>
2021-09-10 15:43:33 +0700

    Add project files.

---
+++ ProjectFrameVB/FormVB.Designer.vb
@@ -0,0 +1,184 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.lblShowText = New System.Windows.Forms.Label()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.txtInputText = New System.Windows.Forms.TextBox()
+        Me.btnClearText = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ColorDialog1 = New System.Windows.Forms.ColorDialog()
+        Me.btnChooseColor = New System.Windows.Forms.Button()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox4 = New System.Windows.Forms.CheckBox()
+        Me.SuspendLayout()
+        '
+        'btnSave
+        '
+        Me.btnSave.Font = New System.Drawing.Font("Leelawadee UI", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSave.Image = Global.ProjectFrameVB.My.Resources.Resources.download
+        Me.btnSave.Location = New System.Drawing.Point(596, 88)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(200, 76)
+        Me.btnSave.TabIndex = 0
+        Me.btnSave.Text = "บันทึกข้อมูล"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'lblShowText
+        '
+        Me.lblShowText.AutoSize = True
+        Me.lblShowText.Font = New System.Drawing.Font("Leelawadee", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblShowText.ForeColor = System.Drawing.Color.Blue
+        Me.lblShowText.Location = New System.Drawing.Point(195, 39)
+        Me.lblShowText.Name = "lblShowText"
+        Me.lblShowText.Size = New System.Drawing.Size(82, 29)
+        Me.lblShowText.TabIndex = 1
+        Me.lblShowText.Text = "Label1"
+        '
+        'btnClose
+        '
+        Me.btnClose.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.btnClose.Location = New System.Drawing.Point(596, 430)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(200, 67)
+        Me.btnClose.TabIndex = 2
+        Me.btnClose.Text = "ปิดโปรแกรม"
+        Me.btnClose.UseVisualStyleBackColor = False
+        '
+        'txtInputText
+        '
+        Me.txtInputText.Font = New System.Drawing.Font("Leelawadee", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtInputText.Location = New System.Drawing.Point(187, 114)
+        Me.txtInputText.Name = "txtInputText"
+        Me.txtInputText.Size = New System.Drawing.Size(373, 28)
+        Me.txtInputText.TabIndex = 3
+        '
+        'btnClearText
+        '
+        Me.btnClearText.Font = New System.Drawing.Font("Leelawadee UI", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClearText.Location = New System.Drawing.Point(596, 330)
+        Me.btnClearText.Name = "btnClearText"
+        Me.btnClearText.Size = New System.Drawing.Size(200, 67)
+        Me.btnClearText.TabIndex = 4
+        Me.btnClearText.Text = "ลบช้อมูล"
+        Me.btnClearText.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.Font = New System.Drawing.Font("Leelawadee", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 20
+        Me.ListBox1.Location = New System.Drawing.Point(187, 181)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(373, 164)
+        Me.ListBox1.TabIndex = 5
+        '
+        'btnChooseColor
+        '
+        Me.btnChooseColor.Location = New System.Drawing.Point(34, 107)
+        Me.btnChooseColor.Name = "btnChooseColor"
+        Me.btnChooseColor.Size = New System.Drawing.Size(75, 36)
+        Me.btnChooseColor.TabIndex = 6
+        Me.btnChooseColor.Text = "เลือกสี"
+        Me.btnChooseColor.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(34, 183)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(66, 21)
+        Me.CheckBox1.TabIndex = 7
+        Me.CheckBox1.Text = "แตงโม"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(34, 211)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(72, 21)
+        Me.CheckBox2.TabIndex = 8
+        Me.CheckBox2.Text = "มะละกอ"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Location = New System.Drawing.Point(34, 239)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(66, 21)
+        Me.CheckBox3.TabIndex = 9
+        Me.CheckBox3.Text = "มะนาว"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'CheckBox4
+        '
+        Me.CheckBox4.AutoSize = True
+        Me.CheckBox4.Location = New System.Drawing.Point(34, 267)
+        Me.CheckBox4.Name = "CheckBox4"
+        Me.CheckBox4.Size = New System.Drawing.Size(59, 21)
+        Me.CheckBox4.TabIndex = 10
+        Me.CheckBox4.Text = "กล้วย"
+        Me.CheckBox4.UseVisualStyleBackColor = True
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(818, 541)
+        Me.Controls.Add(Me.CheckBox4)
+        Me.Controls.Add(Me.CheckBox3)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.btnChooseColor)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.btnClearText)
+        Me.Controls.Add(Me.txtInputText)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.lblShowText)
+        Me.Controls.Add(Me.btnSave)
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "ทดสอบโปรแกรม"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnSave As Button
+    Friend WithEvents lblShowText As Label
+    Friend WithEvents btnClose As Button
+    Friend WithEvents txtInputText As TextBox
+    Friend WithEvents btnClearText As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents ColorDialog1 As ColorDialog
+    Friend WithEvents btnChooseColor As Button
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents CheckBox4 As CheckBox
+End Class
---
+++ ProjectFrameVB/FormVB.vb
@@ -0,0 +1,98 @@
+﻿Public Class frmMain
+
+    Dim vProgram As String = "ทดสอบ"
+    Dim tmpSelect As Integer
+    Dim tmpColor As ColorDialog
+    Dim myStrings() As String
+    Dim myStringElements As Integer = 0
+
+    Private Sub FormVB_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'lblShowText.Text = String.Empty
+        txtInputText.Text = String.Empty
+    End Sub
+
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        Dim result As DialogResult = MessageBox.Show("ต้องการออกจากโปรแกรม ???", vProgram, MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        If result = vbYes Then
+            Me.Close()
+        End If
+        Exit Sub
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        If txtInputText.Text = String.Empty Then
+            MessageBox.Show("กรุณาบันทึกข้อมูล")
+            Exit Sub
+        Else
+            Dim result As DialogResult = MessageBox.Show("ยืนยันบันทึกข้อมูล", vProgram, MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+            If result = vbYes Then
+                lblShowText.Text = txtInputText.Text
+                ListBox1.Items.Add(txtInputText.Text)
+
+
+                lblShowText.Text = String.Empty
+                txtInputText.Text = String.Empty
+                btnSave.BackColor = Color.Red
+
+            End If
+            Exit Sub
+        End If
+    End Sub
+
+    Private Sub btnClearText_Click(sender As Object, e As EventArgs) Handles btnClearText.Click
+        If tmpSelect < 0 Then
+            Exit Sub
+        End If
+        ListBox1.Items.RemoveAt(tmpSelect)
+        btnSave.BackColor = Color.Gray
+        lblShowText.Text = String.Empty
+        txtInputText.Text = String.Empty
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged, ListBox1.Click
+        'MessageBox.Show(ListBox1.SelectedItem)
+        tmpSelect = ListBox1.SelectedIndex
+        'MessageBox.Show(ListBox1.SelectedIndex)
+    End Sub
+
+    Private Sub ListBox1_DoubleClick(sender As Object, e As EventArgs) Handles ListBox1.DoubleClick
+        tmpSelect = ListBox1.SelectedIndex
+        If tmpSelect < 0 Then
+            Exit Sub
+        End If
+        ListBox1.Items.RemoveAt(tmpSelect)
+        lblShowText.Text = String.Empty
+        txtInputText.Text = String.Empty
+    End Sub
+
+    Private Sub btnChooseColor_Click(sender As Object, e As EventArgs) Handles btnChooseColor.Click
+        'ColorDialog1.ShowDialog()
+        If ColorDialog1.ShowDialog <> DialogResult.Cancel Then
+            lblShowText.ForeColor = ColorDialog1.Color
+            txtInputText.ForeColor = ColorDialog1.Color
+        End If
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        'If CheckBox1.Checked = True Then
+        '    ListBox1.Items.Add(CheckBox1.Text)
+        'Else
+        '    ListBox1.Items.RemoveAt(tmpSelect)
+        'End If
+        Dim i As Integer
+        For i = 1 To 5
+            AddElementToStringArray("MyString " & i & ".")
+        Next
+
+        Console.Write(myStrings)
+        Me.lblShowText.Text = ("Number of elements in array is: " & myStrings.Length)
+
+    End Sub
+
+    Public Sub AddElementToStringArray(ByVal stringToAdd As String)
+        ReDim Preserve myStrings(myStringElements)
+        myStrings(myStringElements) = stringToAdd
+        myStringElements += 1
+    End Sub
+
+End Class
